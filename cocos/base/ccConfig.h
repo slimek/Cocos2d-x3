6f8d836ac836109c67772b7ccee2f384548dbcd0
slimek <slime573@gmail.com>
2015-03-10 23:19:42 +0800

    Disable TIFF and WEBP image formats.

--- cocos/base/ccConfig.h
+++ cocos/base/ccConfig.h
@@ -283,14 +283,14 @@ To enable set it to a value different than 0. Disabled by default.
 /** Support TIFF or not. If your application don't use TIFF format picture, you can undefine this macro to save package size.
  */
 #ifndef CC_USE_TIFF
-#define CC_USE_TIFF  1
+// BRITTLE DISABLE: #define CC_USE_TIFF  1
 #endif // CC_USE_TIFF
 
 /** Support webp or not. If your application don't use webp format picture, you can undefine this macro to save package size.
  */
 #ifndef CC_USE_WEBP
 #if (CC_TARGET_PLATFORM != CC_PLATFORM_WP8) && (CC_TARGET_PLATFORM != CC_PLATFORM_WINRT)
-#define CC_USE_WEBP  1
+// BRITTLE DISABLE: #define CC_USE_WEBP  1
 #endif
 #endif // CC_USE_WEBP
 
