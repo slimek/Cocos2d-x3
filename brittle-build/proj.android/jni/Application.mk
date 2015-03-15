dabbca6a2f53e04fa9ac3aca66b2c7c400be10ac
slimek <slime573@gmail.com>
2015-03-15 23:05:23 +0800

    [Android] (minor) Adjust Application.mk.

--- brittle-build/proj.android/jni/Application.mk
+++ brittle-build/proj.android/jni/Application.mk
@@ -3,31 +3,27 @@ NDK_TOOLCHAIN_VERSION := 4.8
 APP_PLATFORM := android-15
 APP_STL := gnustl_static
 
-APP_CFLAGS := -fexceptions
+APP_CFLAGS := -fexceptions -fsigned-char -DCC_ENABLE_CHIPMUNK_INTEGRATION=1
 APP_CPPFLAGS := -std=c++11 -frtti
+APP_LDFLAGS := -latomic
 
-#APP_ABI := x86 armeabi armeabi-v7a
-#APP_ABI := x86
-APP_ABI := armeabi
+APP_ABI := x86 armeabi
 
 
 ifeq ($(NDK_DEBUG),1)
 
 # Debug
 APP_OPTIM := debug
-
 COCOS_CONFIG := gcc48.Debug
 
-APP_CFLAGS += -g -DCOCOS2D_DEBUG=1
+APP_CFLAGS += -DCOCOS2D_DEBUG=1
 
 else
 
 # Release
 APP_OPTIM := release
-
 COCOS_CONFIG := gcc48.Release
 
 endif
-
 
 NDK_APP_OUT := obj/$(COCOS_CONFIG)
